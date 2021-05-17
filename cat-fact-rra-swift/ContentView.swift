24166bfa5a64a1f94168dbacc4bb0fefaf8703d8
Rafael <rrodriguez@hoyosintegrity.com>
2021-05-16 21:05:20 -0400

    Initial Commit

---
+++ cat-fact-rra-swift/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  cat-fact-rra-swift
+//
+//  Created by Rafael Rodriguez on 5/16/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
